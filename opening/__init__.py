c8b051cc4b43d5e28b95107c2bc21b4f19d9e555
Hugo Degeneve <hugo.degeneve@gmail.com>
2024-07-06 17:47:20 +0200

    opening is implemented

--- opening/__init__.py
+++ opening/__init__.py
@@ -1,23 +1,2 @@
-class Node:
-    
-    def __init__(self, move):
-        self.move = move
-        self.children = []
-        self.parents = []
-        self.visits = 0
-        self.successes = 0
-    
-    def add_child(self, child):
-        child_move = self.get_child_move()
-        if child.move not in child_move:
-            self.children.append(child)
-            child.parents.append(self)
-    
-    def get_child_move(self):
-        moves = []
-        for child in self.children:
-            moves.append(child.move)
-        return moves
-        
-    def __eq__(self, other):
-        return self.move == other.move
+from .node import Node
+from .opening import Opening
---
+++ tests/opening_test/node_test.py
@@ -0,0 +1,98 @@
+import chess
+
+import opening
+
+def test_node_eq():
+    root1 = opening.Node()
+    root2 = opening.Node()
+    
+    move1 = chess.Move.from_uci("e2e4")
+    move2 = chess.Move.from_uci("e2e4")
+    
+    root1.add_child(move1)
+    root2.add_child(move2)
+    
+    assert root1 == root2, "The __eq__ method is not working."
+    
+    root1 = opening.Node()
+    root2 = opening.Node()
+    
+    move1 = chess.Move.from_uci("e2e3")
+    move2 = chess.Move.from_uci("e2e4")
+    
+    root1.add_child(move1)
+    root2.add_child(move2)
+    
+    assert root1 == root2, "The __eq__ method is not working."
+
+def test_node_add_child():
+    root = opening.Node()
+    
+    move1 = chess.Move.from_uci("e2e4")
+    move2 = chess.Move.from_uci("e2e4")
+    
+    root.add_child(move1)
+    root.add_child(move2)
+    
+    assert len(root.children) == 1, "The add_child allows for duplicate children."
+
+def test_get_position():
+    move1 = chess.Move.from_uci("e2e4")
+    move2 = chess.Move.from_uci("e7e5")
+    
+    root = opening.Node()
+    leaf1 = root.add_child(move1)
+    leaf2 = leaf1.add_child(move2)
+    
+    position = leaf2.get_position()
+    
+    board = chess.Board()
+    board.push(move1)
+    board.push(move2)
+    true_position = board.fen().split("-")[0]
+    
+    assert position == true_position, "The get_position method is not working."
+
+def test_twin():
+    move1 = chess.Move.from_uci("e2e4")
+    move2 = chess.Move.from_uci("e7e5")
+    
+    move3 = chess.Move.from_uci("e2e3")
+    move4 = chess.Move.from_uci("e7e6")
+    move5 = chess.Move.from_uci("e3e4")
+    move6 = chess.Move.from_uci("e6e5")
+    
+       
+    root = opening.Node()
+    leaf1 = root.add_child(move1)
+    leaf2 = leaf1.add_child(move2)
+    
+    leaf3 = root.add_child(move3)
+    leaf4 = leaf3.add_child(move4)
+    leaf5 = leaf4.add_child(move5)
+    leaf6 = leaf5.add_child(move6)
+    
+    assert leaf2 is leaf6, "The _find_twin method is not working."
+
+def test_delete_child():
+    move1 = chess.Move.from_uci("e2e4")
+    move2 = chess.Move.from_uci("e7e5")
+    
+    root = opening.Node()
+    leaf1 = root.add_child(move1)
+    leaf2 = leaf1.add_child(move2)
+    
+    leaf1.delete_child(move2)
+    
+    assert len(leaf1.children) == 0, "The delete_child method is not working."
+    assert len(leaf2.parents) == 0, "The delete_child method is not working."
+
+def test_back():
+    move1 = chess.Move.from_uci("e2e4")
+    move2 = chess.Move.from_uci("e7e5")
+    
+    root = opening.Node()
+    leaf1 = root.add_child(move1)
+    leaf2 = leaf1.add_child(move2)
+    
+    assert leaf2.back(move2) is leaf1, "The back method is not working."
