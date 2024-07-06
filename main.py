d2fab4cbf4f4ff6c344843ce411f884dec37b50e
Hugo Degeneve <hugo.degeneve@gmail.com>
2024-07-06 18:29:22 +0200

    parser

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import tkinter as tk
+import argparse
 
 import board
 
-root = tk.Tk()
-board_frame = tk.Frame(root)
-chess_board = board.ChessBoard(board_frame, 64)
-
-def start():
+def board_window(args):
+    root = tk.Tk()
+    board_frame = tk.Frame(root)
+    chess_board = board.ChessBoard(board_frame, args.size)
+    if args.fliped:
+        chess_board.flip()
     chess_board.canvas.pack()
     board_frame.pack()
     chess_board.draw()
     root.mainloop()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Chess game")
     
-start()
+    subparsers = parser.add_subparsers(dest="command")
+    
+    board_parser = subparsers.add_parser("board", help="Chess board")
+    board_parser.add_argument("--size", type=int, default=64, help="Size of a square")
+    board_parser.add_argument("--fliped", action="store_true", help="Is the board fliped")
+    board_parser.set_defaults(func=board_window)
+    
+    args = parser.parse_args()
+    args.func(args)
